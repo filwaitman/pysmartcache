323c898c3e496bde67f0064fe67691930a12de1f
Filipe Waitman <filwaitman@gmail.com>
2015-12-13 22:14:10 -0200

    Bump to 0.2

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 setup(
     name='pysmartcache',
     packages=['pysmartcache', ],
-    version='0.1',
+    version='0.2',
     description='PySmartCache is a way to get automatic caching and caching invalidation for functions/methods.',
     author='Filipe Waitman',
     author_email='filwaitman@gmail.com',
     install_requires=open('requirements.txt').readlines(),
     tests_require=open('requirements_test.txt').readlines(),
     url='https://github.com/filwaitman/pysmartcache',
-    download_url='https://github.com/filwaitman/pysmartcache/tarball/0.1',
+    download_url='https://github.com/filwaitman/pysmartcache/tarball/0.2',
     keywords=['cache', 'caching', 'memcached'],
     test_suite='tests',
     classifiers=[
