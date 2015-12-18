de900212f0d0ecec02ee1c0eeaeb811f733b0aa5
Filipe Waitman <filwaitman@gmail.com>
2015-12-18 00:14:38 -0200

    Bump to 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='pysmartcache',
     packages=['pysmartcache', ],
-    version='0.2',
+    version='0.3',
     description='PySmartCache is a way to get automatic caching and caching invalidation for functions/methods.',
     author='Filipe Waitman',
     author_email='filwaitman@gmail.com',
